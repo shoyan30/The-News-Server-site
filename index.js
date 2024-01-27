4c1d31e88cb98c42ebe67720cf4247d63ec1951d
FIRST_NAME LAST_NAME <mdshoyan30@gmail.com>
2024-01-27 16:07:10 +0600

    add server

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const categories = require('./Data/categories.json');
+const news = require('./Data/News.json');
+
+app.use(cors());
+app.get('/', (req, res) => { 
+    res.send('Dragon is running')
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id == id);
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews);
+    }
+
+
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port:${port}`)
+})
